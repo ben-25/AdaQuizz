44c020411892096b7125e14932967dc4a4aad980
antoineLAU <xab97300@gmail.com>
2026-03-10 19:06:20 +0100

    modif

--- src/Dom-acceuil.js
+++ src/Dom-acceuil.js
@@ -2,7 +2,7 @@ import './style.css'
 import quizz from "./quizz-femmes.json"
 
 document.querySelector('#app').innerHTML = `
-<div>
+<div id = ecranAccueil>
     <h1>${quizz.title}</h1>
     <input id= "button" class="styled"  type="button" value="Démarrer" />
     <p class="test"></p>
@@ -12,3 +12,9 @@ const bouton = document.querySelector("#button")
 bouton.addEventListener("click",() => {
     document.querySelector(".test").innerHTML= `bonne chance`;
 })
+
+bouton.addEventListener("click", () => {
+    document.getElementById("ecranAccueil").style.display = "none";
+    document.getElementById("ecranQuestion").style.display = "block";
+});
+
