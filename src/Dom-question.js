e6183c73a93c9205158f4e96eeed636136b81f33
ben-25 <ben-2004@hotmail.fr>
2026-03-10 19:18:47 +0100

    ajout du passage au question suivante

--- src/Dom-question.js
+++ src/Dom-question.js
@@ -1,32 +1,28 @@
 import quiz from "./quizz-femmes-scientifiques.json"
 import './style.css'
 import { check } from "./question-fonction"
+import { activerBtn } from "./question-fonction"
 
 let i = 0;
-const reponse = "test";
 
-document.querySelector('#app').innerHTML = `
+function afficherQuestion(i){
+  document.querySelector('#app').innerHTML = `
   <header>
-    <h1>${quiz.title}</h1>
+  <h1>${quiz.title}</h1>
   </header>
   <main>
-    <div id="question">
+  <div id="question">
         <h2>${quiz.questions[i].question}</h2>
     </div>
     <div id="choix">
-        <input type="button" value="${quiz.questions[i].options[0]}">
+    <input type="button" value="${quiz.questions[i].options[0]}">
         <input type="button" value="${quiz.questions[i].options[1]}">
         <input type="button" value="${quiz.questions[i].options[2]}">
         <input type="button" value="${quiz.questions[i].options[3]}">
     </div>
     <div id="reponse"></div>
   </main>
-`
-document.querySelectorAll('#choix input').forEach((btn) => {
-  btn.addEventListener('click', () => {
-    check(i, quiz.questions[i].correctIndex, btn.value);
-    document.querySelectorAll('#choix input').forEach((b) => {
-      b.disabled = true;
-    });
-  });
-});
+  `
+}
+afficherQuestion(i);
+activerBtn(i, afficherQuestion);
--- src/question-fonction.js
+++ src/question-fonction.js
@@ -1,15 +1,45 @@
 import quiz from "./quizz-femmes-scientifiques.json"
 
 export function check(i, index, value){
+    let nextQuestion;
+    if (i == quiz.questions.length - 1){
+        nextQuestion = `<input type="button" id="end" value="Voir le score">`
+    } else {
+        nextQuestion = `<input type="button" id="next" value="Question suivante">`
+    }
     if (value == quiz.questions[i].options[index]){
         document.querySelector('#reponse').innerHTML = `
         <h2>Bonne reponse</h2>
-        <input type="button" id="next" value="Question suivante">
+        ${nextQuestion}
         `
     } else {
         document.querySelector('#reponse').innerHTML = `
         <h2>Mauvaise reponse</h2>
-        <input type="button" id="next" value="Question suivante">
+        ${nextQuestion}
         `
     }
+}
+
+ export function activerBtn(i, afficherQuestion){
+  document.querySelectorAll('#choix input').forEach((btn) => {
+    btn.addEventListener('click', () => {
+      check(i, quiz.questions[i].correctIndex, btn.value);
+      document.querySelectorAll('#choix input').forEach((b) => {
+        b.disabled = true;
+      });
+      if (next){
+        const next = document.querySelector("#next");
+        next.addEventListener("click", () => {
+          i++;
+          afficherQuestion(i);
+          activerBtn(i, afficherQuestion);
+        })
+      } else if (end){
+        const end = document.querySelector("#end"); //PAGE DE SCORE
+        end.addEventListener("click", () => {
+          console.log("Envoyer vers la page de score")
+        })
+      }
+    });
+  });
 }
