112e6974310585383c01294e7b66e99b24e347cb
antoineLAU <xab97300@gmail.com>
2026-03-10 18:46:47 +0100

    modif

--- src/Dom-acceuil.js
+++ src/Dom-acceuil.js
@@ -3,9 +3,12 @@ import quizz from "./quizz-femmes.json"
 
 document.querySelector('#app').innerHTML = `
 <div>
-
     <h1>${quizz.title}</h1>
-
-</div>
-<input type="button" placeholder="Démarrer">
+    <input id= "button" class="styled"  type="button" value="Démarrer" />
+    <p class="test"></p>
+</div> 
 `
+const bouton = document.querySelector("#button")
+bouton.addEventListener("click",() => {
+    document.querySelector(".test").innerHTML= `bonne chance`;
+})
