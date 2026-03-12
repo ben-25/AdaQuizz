5155bea9b4bd7a346f84b3b3aaecdeb42e5bd753
antoineLAU <xab97300@gmail.com>
2026-03-12 10:38:46 +0100

    copie des code du fichier sur github

--- src/Affichage.js
+++ src/Affichage.js
@@ -1,22 +1,20 @@
 import { domAccueil } from "./Dom-acceuil";
-import { domQuestion } from "./Dom-question"
 import { domFin } from "./Dom-fin";
-
+import { domQuestion } from "./Dom-question";
+import { resetScore } from "./question-fonction";
 
 export function affichage() {
     domAccueil();
-
-    document.addEventListener("click", (e) => {
-        if (e.target.id === "start") {
+    document.addEventListener("click", (btn) => {
+        if (btn.target.id === "start") {
             domQuestion();
         }
-
-        if (e.target.id === "end") {
+        if (btn.target.id === "end") {
             domFin();
         }
-
-        if (e.target.id === "restart") {
-            domAccueil();
+        if (btn.target.id === "restart") {
+            resetScore();
+            affichage();
         }
     });
 }
--- src/question-fonction.js
+++ src/question-fonction.js
@@ -1,5 +1,11 @@
 import quiz from "./quiz-femmes-scientifiques.json"
 
+export let scoreEnd = 0;
+
+export function resetScore(){
+    scoreEnd = 0;   
+}
+
 export function check(i, index, value, score){
     let nextQuestion;
     if (i == quiz.questions.length - 1){
@@ -8,7 +14,7 @@ export function check(i, index, value, score){
         nextQuestion = `<input type="button" id="next" value="Question suivante">`
     }
     if (value == quiz.questions[i].options[index]){
-        score++;
+        scoreEnd++;
         document.querySelector('#reponse').innerHTML = `
         <h2>Bonne reponse</h2>
         ${nextQuestion}
@@ -19,30 +25,25 @@ export function check(i, index, value, score){
         ${nextQuestion}
         `
     }
-    return score;
+    return scoreEnd;
 }
 
 export function activerBtn(i, afficherQuestion, score){
     document.querySelectorAll('#choix input').forEach((btn) => {
         btn.addEventListener('click', () => {
             check(i, quiz.questions[i].correctIndex, btn.value, score);
-            score = check(i, quiz.questions[i].correctIndex, btn.value, score);
             document.querySelectorAll('#choix input').forEach((b) => {
             b.disabled = true;
-        });
-        const next = document.querySelector("#next");
-        const end = document.querySelector("#end");
-        if (next){
-            next.addEventListener("click", () => {
-                i++;
-                afficherQuestion(i);
-                activerBtn(i, afficherQuestion, score);
-            })
-        } else if (end){
-            end.addEventListener("click", () => {
-                return score;
-            })
-        }
+            });
+            const next = document.querySelector("#next");
+            const end = document.querySelector("#end");
+            if (next){
+                next.addEventListener("click", () => {
+                    i++;
+                    afficherQuestion(i);
+                    activerBtn(i, afficherQuestion, score);
+                })
+            }
         });
     });
 }
