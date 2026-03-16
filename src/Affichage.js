95b7e8fccb1e3e368761c3228d9d78663e1b4beb
ben-25 <ben-2004@hotmail.fr>
2026-03-16 10:01:21 +0100

    idk

--- src/Affichage.js
+++ src/Affichage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { domAccueil } from "./Dom-accueil";
 import { domFin } from "./Dom-fin";
 import { domQuestion } from "./Dom-question";
@@ -19,27 +18,3 @@ export function affichage() {
         }
     });
 }
-=======
-import { domAccueil } from "./Dom-acceuil";
-import { domQuestion } from "./Dom-question"
-import { domFin } from "./Dom-fin";
-
-
-export function affichage() {
-    domAccueil();
-
-    document.addEventListener("click", (e) => {
-        if (e.target.id === "start") {
-            domQuestion();
-        }
-
-        if (e.target.id === "end") {
-            domFin();
-        }
-
-        if (e.target.id === "restart") {
-            domAccueil();
-        }
-    });
-}
->>>>>>> 0c35982103b75f62cc18b4907d4fbf23d989c281
